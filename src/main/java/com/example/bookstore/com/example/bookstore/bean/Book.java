9e9539b1ad31c93ec2bf2917a3bc4071095d5d81
Aleksi Vassiliev <pekkahirvesa@hotmail.fi>
2017-02-02 16:03:38 +0200

    first commit

---
+++ src/main/java/com/example/bookstore/com/example/bookstore/bean/Book.java
@@ -0,0 +1,66 @@
+package com.example.bookstore.com.example.bookstore.bean;
+
+public class Book {
+
+    private String title, author, year, isbn;
+    private Double price;
+
+    public Book(String title, String author, String year, String isbn, Double price) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year='" + year + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
